4f676ca34afe3eedc4ad516d4dbda1901e20bb73
leydevitm <leivy.candelaria@devitm.com>
2025-08-06 12:14:36 -0600

    50 Props

--- seccion4/components-1/src/App.vue
+++ seccion4/components-1/src/App.vue
@@ -1,17 +1,27 @@
 
 <script setup>
+import BlogPost from './components/BlogPost.vue';
 import ButtonCounter from './components/ButtonCounter.vue';
 </script>
 
 
 <template>
   <div class="container">
-  <h1>Leivy</h1>
-  <ButtonCounter></ButtonCounter>
- 
- <button-counter></button-counter>
- </div>
+    <h1>Leivy</h1>
+    <ButtonCounter></ButtonCounter>
+    <BlogPost
+    title="Post 1"
+    />
+     <BlogPost
+    title="Post 2"
+    />
+     <BlogPost
+    title="Post 3"
+    />
+  </div>
 </template>
+
+
 
 
 
---
+++ seccion4/components-1/src/components/BlogPost.vue
@@ -0,0 +1,34 @@
+<script setup>
+defineProps(['title'])
+</script>
+
+<template>
+    <div class="card">
+        <div class="card-body">
+            <h5 class="card-title">{{ title }}</h5>
+            <p class="card-text">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            </p>
+        </div>
+    </div>
+</template>
+
+
+<style>
+.card {
+    margin-bottom: 1rem;
+}
+.card-body {
+    background-color: #f8f9fa;
+    border: 1px solid #dee2e6;
+    border-radius: 0.25rem;
+}
+.card-title {
+    font-size: 1.25rem;
+    font-weight: 500;
+}
+.card-text {
+    font-size: 1rem;
+    color: #6c757d;
+}
+</style>
