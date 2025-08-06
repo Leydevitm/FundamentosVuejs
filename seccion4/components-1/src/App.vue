25e5c15dd15fe190108e9c2749203943e90694ff
leydevitm <leivy.candelaria@devitm.com>
2025-08-05 20:52:27 -0600

    primer componente

---
+++ seccion4/components-1/src/App.vue
@@ -0,0 +1,17 @@
+
+<script setup>
+import ButtonCounter from './components/ButtonCounter.vue';
+</script>
+
+
+<template>
+  <div class="container">
+  <h1>Leivy</h1>
+  <ButtonCounter></ButtonCounter>
+ 
+ <button-counter></button-counter>
+ </div>
+</template>
+
+
+
---
+++ seccion4/components-1/src/components/ButtonCounter.vue
@@ -0,0 +1,27 @@
+<script setup>
+import { ref } from 'vue';
+
+
+const counter = ref(0);
+const increment = () => {
+  counter.value++;
+};
+</script>
+
+<template>
+    <button @click="increment">Counter{{ counter }}</button>
+</template>
+
+<style>
+button {
+  background-color: blue;
+  color: white;
+  border: none;
+  padding: 10px 20px;
+  cursor: pointer;
+}
+
+button:hover {
+  background-color: darkblue;
+}
+</style>
