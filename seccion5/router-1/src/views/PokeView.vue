b0a396ba9ccc58d4677c8568192376bf9961c0c5
leydevitm <leivy.candelaria@devitm.com>
2025-08-07 17:31:33 -0600

    Clase 84 Pintar informacion del Pokemon

--- seccion5/router-1/src/views/PokeView.vue
+++ seccion5/router-1/src/views/PokeView.vue
@@ -1,13 +1,25 @@
 <script setup>
 
 import axios from 'axios'
+import { useRoute } from 'vue-router'
+import { useRouter } from 'vue-router'
 import { ref } from 'vue'
 
+const poke=ref({})
+
+
+const route = useRoute()
+const router = useRouter()
+
+const back=()=>{
+    router.push('/Pokeemons')
+}
 const getData = async () => {
   try {
-    const { data } = await axios.get(`https://pokeapi.co/api/v2/pokemon/pikachu`)
+    const { data } = await axios.get(`https://pokeapi.co/api/v2/pokemon/${route.params.name}`)
     // pokemons.value = data.results
     console.log(data);
+    poke.value=data;
   } catch (error) {
     console.error('Error fetching Pokemons:', error)
   }
@@ -17,5 +29,7 @@ getData();
 </script>
 
 <template>
+    <img :src="poke.sprites?.front_default">
     <h1>Poke name: {{ $route.params.name }}</h1>
+    <button @click="back">Volver</button>
 </template>
