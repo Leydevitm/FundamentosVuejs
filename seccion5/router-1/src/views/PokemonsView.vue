ad0634d185eab0607e31394a7412e306d3d1eca8
leydevitm <leivy.candelaria@devitm.com>
2025-08-07 15:13:34 -0600

    Nueva ruta usando axios

---
+++ seccion5/router-1/src/views/PokemonsView.vue
@@ -0,0 +1,26 @@
+<script setup>
+import axios from 'axios'
+import { ref } from 'vue'
+
+const pokemons = ref([])
+const getData=async () => {
+  try {
+    const {data} = await axios.get('https://pokeapi.co/api/v2/pokemon')
+   
+    pokemons.value = data.results
+  } catch (error) {
+    console.error('Error fetching Pokemons:', error)
+  }
+
+}
+getData();
+</script>
+
+<template>
+    <h1>Pokeemons</h1>
+    <ul>
+        <li v-for="poke in pokemons">
+            {{ poke.name }}
+        </li>
+    </ul>
+</template>
