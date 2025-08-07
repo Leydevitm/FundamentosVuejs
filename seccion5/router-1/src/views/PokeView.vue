eedaffe778da427e76fb2308b95897c79ef59f08
leydevitm <leivy.candelaria@devitm.com>
2025-08-07 15:41:19 -0600

    Clase 80 Axios traer unico pokemon

--- seccion5/router-1/src/views/PokeView.vue
+++ seccion5/router-1/src/views/PokeView.vue
@@ -1,3 +1,21 @@
+<script setup>
+
+import axios from 'axios'
+import { ref } from 'vue'
+
+const getData = async () => {
+  try {
+    const { data } = await axios.get(`https://pokeapi.co/api/v2/pokemon/pikachu`)
+    // pokemons.value = data.results
+    console.log(data);
+  } catch (error) {
+    console.error('Error fetching Pokemons:', error)
+  }
+}
+
+getData();
+</script>
+
 <template>
     <h1>Poke name: {{ $route.params.name }}</h1>
 </template>
