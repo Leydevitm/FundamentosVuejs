126c8e4b9738fc80c9a15db9af612a7b247d460c
leydevitm <leivy.candelaria@devitm.com>
2025-08-13 10:09:09 -0600

    Clase 108 vite + Routerv4

---
+++ seccion8/firebase-app-1/src/App.vue
@@ -0,0 +1,15 @@
+import { RouterView } from 'vue-router';
+<script setup>
+
+</script>
+<template>
+  <div>
+    <h1>App</h1>
+    <router-link to="/">Home</router-link>
+    <router-link to="/login">Login</router-link>
+    <router-link to="/register">Register</router-link>
+    <button>Logout</button>
+    <router-view></router-view>
+    </div>
+</template>
+
