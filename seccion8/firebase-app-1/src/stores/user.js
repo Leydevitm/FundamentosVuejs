bf0476a15c7a972123e1dc6276df01c3a68e1203
leydevitm <leivy.candelaria@devitm.com>
2025-08-14 18:36:27 -0600

    Clase 115 bloquear boton login/register

--- seccion8/firebase-app-1/src/stores/user.js
+++ seccion8/firebase-app-1/src/stores/user.js
@@ -11,9 +11,11 @@ import router from '../router';
 export const useUserStore = defineStore('userStore',{
     state:()=>({
         userData:null,
+        loadingUser:false
     }),
     actions: {
         async registerUser(email,password){
+            this.loadingUser = true;
             try {
                 const {user} = await createUserWithEmailAndPassword(
                     auth,
@@ -24,19 +26,24 @@ export const useUserStore = defineStore('userStore',{
                router.push('/');
             } catch (error) {
                 console.log(error);
+            }finally {
+                this.loadingUser = false;
             }
-
         },
         async loginUser(email,password){
+            this.loadingUser = true;
             try {
             const {user} = await signInWithEmailAndPassword(auth,email,password)
              this.userData={email:user.email,uid:user.uid}
               router.push('/');
             } catch (error) {
                 console.log(error)
+            }finally {
+                this.loadingUser = false;
             }
         },
         async logoutUser(){
+            
             try {
                 await signOut(auth);
                 this.userData = null;
--- seccion8/firebase-app-1/src/views/Register.vue
+++ seccion8/firebase-app-1/src/views/Register.vue
@@ -25,7 +25,7 @@ const handleSubmit =async ()=>{
        <form @submit.prevent="handleSubmit" action="">
         <input type="email" placeholder="Email" v-model="email">
         <input type="password" placeholder="Password" v-model="password">
-        <button type="submit">Submit</button>
+        <button type="submit" :disabled="userStore.loadingUser">Submit</button>
        </form>
     </div>
 
