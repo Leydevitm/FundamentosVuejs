8c331b37b44818edb5fea7747f58cda2c6e7dded
leydevitm <leivy.candelaria@devitm.com>
2025-08-19 15:21:44 -0600

    Clase 133 Loading Button

--- seccion8/firebase-app-1/src/views/Login.vue
+++ seccion8/firebase-app-1/src/views/Login.vue
@@ -75,7 +75,7 @@ const onFinishFailed = (errorInfo: any) => {
         </a-form-item>
 
         <a-form-item>
-            <a-button type="primary" html-type="submit" :disabled="userStore.loadingUser">Ingresar</a-button>
+            <a-button type="primary" html-type="submit" :disabled="userStore.loadingUser" :loading="userStore.loadingUser">Ingresar</a-button>
         </a-form-item>
 
        </a-form>
