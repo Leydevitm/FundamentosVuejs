3d8d1d762051fa5faa0fac0555b1afb99b7d824f
leydevitm <leivy.candelaria@devitm.com>
2025-08-21 22:07:00 -0600

    Clase 157 Backend Node.js y express

---
+++ seccion9/express/backend-twitch/models/User.js
@@ -0,0 +1,22 @@
+import mongoose from 'mongoose';
+import {Schema,model}from 'mongoose';
+
+const userSchema = new Schema({
+    
+    email: { 
+        type: String, 
+        required: true,
+        trim:true,
+        unique:true,
+        lowercase:true,
+        index:{unique:true}
+    },
+    password: { 
+        type: String, 
+        required: true,
+        
+    }
+});
+
+const User = model('user', userSchema);
+export default User;
