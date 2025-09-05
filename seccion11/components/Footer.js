444112690693140d34dcfdac5aff2ad35a44a760
leydevitm <leivy.candelaria@devitm.com>
2025-09-05 10:34:49 -0600

    VUE 3 OPTION API

---
+++ seccion11/components/Footer.js
@@ -0,0 +1,17 @@
+app.component('footer-banco', {
+        props: ['cantidad','fecha'],
+    template:/*html*/ `
+        <div class="bg-dark py-3 mt-2 text-white">
+            <h3>{{ texto }} - {{cantidad}}</h3>
+            <p>{{ fecha }}</p>
+        </div>
+    `,
+    data(){
+        return {
+            texto: "Este es el footer",
+           
+        }
+    }
+
+});
+
--- seccion11/main.js
+++ seccion11/main.js
@@ -1,7 +1,39 @@
 const app = Vue.createApp({
     data(){
         return {
-            titulo: "Hola Mundo desde Vue"
+            titulo: "Mi banco con VUE.js",
+            cantidad:1000,
+            enlace: "https://youtube.com/bluuweb",
+            estado:true,
+            servicios: [
+                'transferencias',
+                'pagos',
+                'giros'
+            ],
+            desactivar:false
+        }
+    },
+    methods: {
+        agregarSalto() {
+            this.cantidad += 100;
+        },
+        disminuirSalto(valor){
+            if(this.cantidad ===0){
+                this.desactivar =true
+                alert('salto en 0');
+                
+                return 
+            }
+
+            this.cantidad= this.cantidad -valor;
+        }
+    },
+    computed:{
+        colorCantidad(){
+            return this.cantidad > 500 ? 'text-success' : 'text-danger'
+        },
+        mayusculaTexto(){
+            return this.titulo.toUpperCase();
         }
     }
 });
