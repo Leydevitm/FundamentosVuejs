1b3c9aa49ebddb3a4ebf82105159dc4bac963301
leydevitm <leivy.candelaria@devitm.com>
2025-09-08 10:38:31 -0600

    Clase 204 Creando nuestro primer componente

---
+++ seccion12/hello-world/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+
+
+  <img alt="Vue logo" src="./assets/logo.png">
+  <titulo texto="Mi banco dinamico 2.0"></titulo>
+    </div>
+</template>
+
+<script>
+// import HelloWorld from './components/HelloWorld.vue'
+import Titulo from './components/Titulo.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Titulo
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ seccion12/hello-world/src/components/Titulo.vue
@@ -0,0 +1,19 @@
+<template>
+  <h1>{{ texto }}</h1>
+</template>
+
+<script>
+export default {
+    name:'Titulo',
+    props:{
+        texto:String
+    }
+
+}
+</script>
+
+<style scope>
+h1{
+    color:blue;
+}
+</style>
