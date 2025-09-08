276e481da06dc861069b6463514697203199fae5
leydevitm <leivy.candelaria@devitm.com>
2025-09-08 14:36:37 -0600

    Clase 222 Practica Rutas con parametros

--- seccion13/router-1/src/views/Articulo.vue
+++ seccion13/router-1/src/views/Articulo.vue
@@ -2,6 +2,8 @@
  <div>
     <Titulo texto="Ruta con parametros"></Titulo>
     <h2>Parametro: {{ $route.params.id }}</h2>
+    <h3>{{ articulo.title }}</h3>
+    <p>{{ articulo.id }}.- {{ articulo.body }}</p>
   </div>
 </template>
 
@@ -10,7 +12,27 @@ import Titulo from '../components/Titulo.vue'
 export default {
     components:{
         Titulo,
-    }
+    },
+    data(){
+        return{
+            articulo:{}
+        }
+    },
+    methods:{
+        async consumirArticulo(){
+        try {
+            const data = await fetch (`https://jsonplaceholder.typicode.com/posts/${this.$route.params.id}`)
+            const objeto = await data.json()
+            console.log(objeto);
+            this.articulo= objeto
+        } catch (error) {
+            console.log(error);
+        }
+        }
+    },
+    created(){
+        this.consumirArticulo()
+    },
 
 }
 </script>
--- seccion13/router-1/src/views/Blog.vue
+++ seccion13/router-1/src/views/Blog.vue
@@ -1,13 +1,44 @@
 <template>
-  <Titulo texto="Titulo de mi blog" ></Titulo>
+  <div>
+    <Titulo texto="Titulo de mi blog" ></Titulo>
+    <div v-for="item in arrayBlog" :key="item.id">
+    <router-link :to="`/blog/${item.id}`">
+    {{ item.title }}
+    </router-link>
+    </div>
+      
+  
+  </div>
+ 
 </template>
 
 <script>
 import Titulo from '../components/Titulo.vue'
+import { Router, RouterLink } from 'vue-router';
 export default {
 
     components:{
         Titulo,
+    },
+    data(){
+        return {
+            arrayBlog:[]
+        }
+    },
+    methods:{
+     async consumirApi(){
+    try {
+        const data = await fetch('https://jsonplaceholder.typicode.com/posts')
+        const array = await data.json()
+        console.log(array);
+        this.arrayBlog=array;
+    } catch (error) {
+        console.log(error);
+    }
+    }
+    },
+    created(){
+     this.consumirApi()
     }
 }
 </script>
