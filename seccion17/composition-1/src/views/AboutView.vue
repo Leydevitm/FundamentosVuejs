e03f1945fc094d8f9717ff082a91952427f314d6
leydevitm <leivy.candelaria@devitm.com>
2025-09-09 11:11:34 -0600

    Clase 296 Propiedades computadas

---
+++ seccion17/composition-1/src/views/AboutView.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="about">
+    <h1 :style="{'color':color}">Contador: {{ contador }}</h1>
+    <button @click="aumentar()">+</button>
+     <button @click="disminir()">-</button>
+  </div>
+</template>
+
+<script >
+import {computed, ref} from 'vue'
+ export default{
+  setup(){
+    
+    const contador = ref(0);
+    const aumentar =()=>{
+      contador.value++
+    }
+    const disminir =()=>{
+      contador.value--
+    }
+
+    const color = computed(()=>{
+    if(contador.value <= 0){
+      return 'red'
+    }else{
+      return 'blue'
+    }
+    })
+
+  return {contador,aumentar,disminir,color}
+    }
+  
+  }
+ 
+</script>
---
+++ seccion17/composition-1/src/views/HomeView.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="home">
+   <h1 :style="{'color':color}">Contador: {{ contador }}</h1>
+   <button @click="aumentar()">Amentar</button>
+   <button @click="disminuir()">Disminuir</button>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'HomeView',
+  components: {
+    
+  },
+  data(){
+    return{
+      contador: 0
+    }
+  },
+  computed:{
+   color(){
+    if(this.contador <= 0){
+      return 'red'
+    }else{
+      return 'green'
+    }
+   }
+  },
+  methods:{
+    aumentar(){
+      this.contador ++ 
+    },
+    disminuir(){
+      this.contador--
+    }
+  }
+  
+}
+</script>
