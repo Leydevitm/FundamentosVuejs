a84d3c5962655215127a6796e5860efdfbbc1528
leydevitm <leivy.candelaria@devitm.com>
2025-09-08 21:09:45 -0600

    Clase 246 -model: text

---
+++ seccion15/formulario-1/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+ <div>
+<Navbar></Navbar>
+<div class="container"></div>
+    <router-view/>
+ </div>
+
+</template>
+
+<script>
+import Navbar from './components/Navbar.vue'
+export default{
+  components:{
+    Navbar
+  },
+ 
+}
+</script>
---
+++ seccion15/formulario-1/src/views/HomeView.vue
@@ -0,0 +1,26 @@
+<template>
+ <div>
+   <form action="">
+     <input 
+       type="text"
+       class="form-control my-2"
+       placeholder="Ingrese nombre"
+       v-model="tarea.nombre">
+   </form>
+   <p>{{ tarea }}</p>
+ </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeView',
+  data() {
+    return {
+      texto: '',
+      tarea:{
+        nombre: ''
+      }
+    }
+  }
+}
+</script>
