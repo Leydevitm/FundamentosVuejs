9ce98aa79cfa8bfb81ea3beb668d6cae41b9f530
leydevitm <leivy.candelaria@devitm.com>
2025-09-08 22:20:51 -0600

    Clase 257 Separar inputs en un componente

--- seccion15/formulario-1/src/views/HomeView.vue
+++ seccion15/formulario-1/src/views/HomeView.vue
@@ -1,57 +1,7 @@
 <template>
  <div>
    <form @submit.prevent="procesarFormulario">
-     <input 
-       type="text"
-       class="form-control my-2"
-       placeholder="Ingrese nombre"
-       v-model="tarea.nombre">
-  
-
-   <div class="form-check form-check-inline">
-    <input type="checkbox"
-    id="check-1"
-    class="form-check-input"
-    v-model="tarea.categorias"
-    value="javascript">
-    <label for="check-1" class="form-check-label">javascript</label>
-   </div>
-   <div class="form-check form-check-inline">
-    <input type="checkbox"
-    id="check-2"
-    class="form-check-input"
-    v-model="tarea.categorias"
-    value="node js">
-    <label for="check-2" class="form-check-label">Node js</label>
-   </div>
-
-   <div class="mt-2">
-    <div class="form-check form-check-inline">
-     <input type="radio" id="radio-1" 
-     class="form-check-input"
-     value="urgente"
-     v-model="tarea.estado">
-     <label for="radio-1" class="form-check-label">Urgente</label>
-    </div>
-
-    <div class=" form-check-inline">
-     <input type="radio" id="radio-2"
-     class="form-check-input"
-     value="relax"
-      v-model="tarea.estado">
-     <label for="radio-2" class="form-check-label">Relax</label>
-    </div>
-   </div>
-   <div class="mt-2">
-    <input type="number"
-    class="form-control"
-    v-model.number="tarea.numero">
-   </div>
-   <button 
-   class="btn btn-dark mt-2 btn-block" 
-   type="submit"
-   :disabled="bloquear"
-   >Procesar </button>
+     <Input :tarea="tarea"></Input>
    </form>
     <hr>
      <p>{{ tarea }}</p>
@@ -59,12 +9,16 @@
 </template>
 
 <script>
+import Input from '../components/Input.vue'
 export default {
   name: 'HomeView',
+  components:{
+  Input
+  },
   data() {
     return {
-      texto: '',
       tarea:{
+        id:'',
         nombre: '',
         categorias:[],
         estado:'',
@@ -89,10 +43,6 @@ export default {
       }
     }
   },
-  computed:{
-    bloquear(){
-    return this.tarea.nombre.trim() === "" ? true : false
-    }
-  }
+  
 }
 </script>
