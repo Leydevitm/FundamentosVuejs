9c1b5de44b9ae583ee47783779f73d9e3bd202a5
leydevitm <leivy.candelaria@devitm.com>
2025-09-09 11:31:10 -0600

    Clase 298 Props

--- seccion17/composition-1/src/views/AboutView.vue
+++ seccion17/composition-1/src/views/AboutView.vue
@@ -1,17 +1,26 @@
 <template>
   <div class="about">
-    <h1 :style="{'color':color}">Contador: {{ contador }}</h1>
+   <Titulo :contador="contador" :color="color"></Titulo>
+   <!-- <h1 :style="{'color':color}">Contador: {{ contador }}</h1> -->
     <button @click="aumentar()">+</button>
      <button @click="disminir()">-</button>
+     <hr>
+     <input type="text" v-model="texto">
+     <p>{{ texto }}</p>
   </div>
 </template>
 
 <script >
+import Titulo from '../components/Titulo.vue'
 import {computed, ref} from 'vue'
  export default{
+  components:{
+    Titulo,
+  },
   setup(){
     
     const contador = ref(0);
+    const texto = ref('')
     const aumentar =()=>{
       contador.value++
     }
@@ -27,7 +36,7 @@ import {computed, ref} from 'vue'
     }
     })
 
-  return {contador,aumentar,disminir,color}
+  return {contador,aumentar,disminir,color,texto}
     }
   
   }
