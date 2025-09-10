88d54e90b99d3d1f09a1ad8c950f45b9deb11555
leydevitm <leivy.candelaria@devitm.com>
2025-09-10 11:21:21 -0600

    Clase 328 watchEffect en vue3

--- seccion19/provide2-app/src/components/TareaApp.vue
+++ seccion19/provide2-app/src/components/TareaApp.vue
@@ -19,7 +19,7 @@
 <script>
 import TareaItem from './TareaItem.vue'
 import TareaForm from './TareaForm.vue'
-import { provide, ref } from 'vue';
+import { provide, ref, watchEffect } from 'vue';
 
 export default {
     components:{
@@ -31,6 +31,12 @@ export default {
     const tareas = ref([])
 
     provide('tareas', tareas)
+    if(localStorage.getItem('tareas')){
+        tareas.value = JSON.parse(localStorage.getItem('tareas'))
+    }
+     watchEffect(()=>{
+        localStorage.setItem('tareas', JSON.stringify(tareas.value))
+    })
 
     return{tareas}
    }
