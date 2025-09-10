2c5c4a2e5a606c70ff61cf85671efdea6b2aa691
leydevitm <leivy.candelaria@devitm.com>
2025-09-10 10:27:34 -0600

    Clase 325 Componente Formulario Provide e inject

---
+++ seccion19/provide2-app/src/components/TareaApp.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <h1>App Tareas</h1>
+    <TareaForm></TareaForm>
+  <p>  {{ tareas }}</p>
+  </div>
+</template>
+
+<script>
+import TareaForm from './TareaForm.vue'
+import { provide, ref } from 'vue';
+
+export default {
+    components:{
+        TareaForm,
+    },
+   setup(){
+
+    const tareas = ref([])
+
+    provide('tareas', tareas)
+
+    return{tareas}
+   }
+}
+</script>
+
+<style>
+
+</style>
---
+++ seccion19/provide2-app/src/components/TareaForm.vue
@@ -0,0 +1,48 @@
+<template>
+  <div>
+    <form @submit.prevent="agregarTarea">
+        <input 
+        type="text"
+        placeholder="Ingrese tarea"
+        class="form-control my-2"
+        v-model.trim="texto">
+
+        <button class="btn btn-primary w-100">Agregar</button>
+    </form>
+  </div>
+</template>
+
+<script>
+ import {inject , ref} from 'vue'
+export default {
+   
+    setup(){
+        const texto = ref('')
+        const tareas = inject('tareas')
+
+        const agregarTarea=()=>{
+            if(texto.value === ''){
+              return console.log('texto vacio')
+            }
+            const tarea = {
+                texto: texto.value,
+                estado: false,
+                id: Date.now()
+            }
+
+            tareas.value.push(tarea)
+            texto.value =''
+
+        }
+        return{
+         agregarTarea,
+         texto
+        }
+    }
+
+}
+</script>
+
+<style>
+
+</style>
