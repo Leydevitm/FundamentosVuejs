30f5d7bab0549771e9c8aeb7171c9b71c9de0595
leydevitm <leivy.candelaria@devitm.com>
2025-09-11 12:51:11 -0600

    Clase 373 Agregar documentos a firestore

---
+++ seccion21/crudd/src/firebase.js
@@ -0,0 +1,17 @@
+import firebase from "firebase/app";
+import firestore from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAIlMfr07Ks1DFlURQaCi5TkiIDNKUj9pU",
+  authDomain: "crud-1-e1d37.firebaseapp.com",
+  projectId: "crud-1-e1d37",
+  storageBucket: "crud-1-e1d37.firebasestorage.app",
+  messagingSenderId: "416292508122",
+  appId: "1:416292508122:web:9d0c4fa4bf0758aebcf0e3"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+const db = firebase.firestore();
+
+export { db };
---
+++ seccion21/crudd/src/store/index.js
@@ -0,0 +1,67 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+import {db} from '../firebase'
+import router from '../router'
+
+Vue.use(Vuex)
+export default new Vuex.Store({
+ state: {
+    tareas: [],
+    tarea: {nombre:'',id: ''}
+},
+mutations: {
+    setTareas(state, payload){
+        state.tareas = payload
+    },
+     setTarea(state, payload){
+        state.tarea = payload
+    }
+},
+actions: {
+    getTareas({commit}){
+        const tareas = []
+        db.collection('tareas').get()
+        .then(res => {
+            res.forEach(doc => {
+                console.log(doc.id)
+                console.log(doc.data())
+                let tarea = doc.data()
+                tarea.id = doc.id
+                tareas.push(tarea)
+            })
+            commit('setTareas', tareas)
+        })
+    },
+
+    getTarea({commit,idTarea}){
+      db.collection('tareas').doc(idTarea).get()
+      .then(doc =>{
+        console.log(doc.id)
+        console.log(doc.data())
+        let tarea = doc.data()
+        tarea.id= doc.id
+        commit('setTare', tarea)
+      })
+    },
+    editarTarea({commit}, tarea){
+        db.collection('tareas').doc(tarea.id).update({
+        nombre: tarea.nombre
+    })
+    .then(() => {
+        router.push('/')
+    })
+    },
+    agregarTarea({commit}, nombreTarea){
+          db.collection('tareas').add({
+        nombre: nombreTarea
+    })
+    .then(doc => {
+        console.log(doc.id)
+        router.push('/')
+    })
+    }
+    
+    
+
+}
+})
